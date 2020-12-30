9fa6db2ffb6fb4ad6d03e4f1dda8092a0fb00225
TrinhDuy94 <72688837+TrinhDuy94@users.noreply.github.com>
2020-12-30 19:17:30 +0700

    fix

--- client.py
+++ client.py
@@ -9,15 +9,14 @@ def main():
 	print('connecting to %s port ' + str(server_address))
 	s.connect(server_address)
 	try:
-    		while True:
-        		msg = input('Client: ')
-        		s.sendall(bytes(msg, "utf8"))
+		while True:
+			msg = input('Client: ')
+			s.sendall(bytes(msg, "utf8"))
 			if msg == "quit":
 				break
-
-        	data = s.recv(1024)
-        	print('Server: ', data.decode("utf8"))
+		data = s.recv(1024)
+		print('Server: ', data.decode("utf8"))
 	finally:
-    		s.close()
+		s.close()
 
 main()
